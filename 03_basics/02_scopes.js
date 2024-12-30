4189e8a63a31fae67d0c0013b770d72d6cc6dcdc
Sakshi Jadhav <88924554+Sakshijadhav77@users.noreply.github.com>
2024-12-30 08:24:17 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,18 +5,57 @@ let a=300
  if(true){
     let a=10
     const b=20
-    console.log("INNER: ",a);
+   //  console.log("INNER: ",a);
  }
  //curly braces ke ander is block scope
  //outside is global scope
 
 
- for (let i = 0; i < array.length; i++) {
-    const element = array[i];
+//  for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
     
- }
+//  }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+   const username ='sakshi'
+
+   function two(){
+      const website='youtube'
+      // console.log(username);
+      
+   }
+   // console.log(website);
+   two()
+   
+}
+one()
+
+if(true){
+   const username="sakshi"
+   if(username==="sakshi"){
+      const website=' youtube'
+      // console.log(username+website);
+      
+   }
+   // console.log(website);
+   
+}
+// console.log(username);
+
+//++++++++interesting+++++++++++++++++
+function addone(num){
+   return num + 1
+}
+addone(5)
+const addtwo=function(num){  //way 2 of creating a function
+   return num + 2
+}
+addtwo(5)
+
+
 
